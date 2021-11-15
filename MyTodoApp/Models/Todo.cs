c4c23dabfaa3935a41a1efe39ab99a13a44535a0
Yuri Fernandes <yuri.fernandes@primecontrol.com.br>
2021-11-15 10:42:19 -0300

    Adiciona lista de Todo com login

---
+++ MyTodoApp/Models/Todo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyTodoApp.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Título")]
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public string Title { get; set; }
+
+        [DisplayName("Concluído")]
+        public bool Done { get; set; }
+
+        [DisplayName("Criado em")]
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        [DisplayName("Última atualização")]
+        public DateTime LastUpdateDate { get; set; } = DateTime.Now;
+
+        [DisplayName("Usuário")]
+        public string User { get; set; }
+    }
+}
